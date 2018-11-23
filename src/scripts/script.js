d4d24561e734919dd642063f0d8c122026c6bed7
Keran Braich <keran.braich@ft.com>
2018-11-23 17:32:50 +0000

    Update error message

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -2,6 +2,6 @@ var uploadInput = document.getElementById('upload-cv');
 
 uploadInput.addEventListener('change', function(event) {
     if (uploadInput.files[0].size / 1024 >= 300) {
-        alert('We have so many applications, and so little space to store them. Please provide a smaller version of your CV which size would be less that 300KB. Thank you for your understanding.');
+        alert('We have so many applications, and so little space to store them!  Please provide a smaller file version, less than 300kb.  If this is a problem, please let us know you were unable to send your CV when we respond to your form submission');
     }
 });
